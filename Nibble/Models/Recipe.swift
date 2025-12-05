cab722b7347ffa3b1a0e0b214319175ac6d8a612
yashibuilds <yashibuilds@gmail.com>
2025-12-05 13:54:36 -0500

    Modify initializer

--- Nibble/Models/Recipe.swift
+++ Nibble/Models/Recipe.swift
@@ -4,6 +4,16 @@ struct Recipe {
     var mainInformation: MainInformation
     var ingredients: [Ingredient]
     var directions: [Direction]
+    
+    init(mainInformation: MainInformation, ingredients: [Ingredient], directions: [Direction]) {
+        self.mainInformation = mainInformation
+        self.ingredients = ingredients
+        self.directions = directions
+    }
+    
+    init() {
+        self.init(mainInformation: MainInformation(name: "", description: "", author: "", category: .breakfast), ingredients: [], directions: [])
+    }
 }
 
 struct MainInformation {
