cb94a43b8bf8e940b278d69096154fa3a2e333bc
yashibuilds <yashibuilds@gmail.com>
2025-12-04 23:32:05 -0500

    Initial Commit

---
+++ Nibble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nibble
+//
+//  Created by Yashi Surapaneni on 12/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nibble/NibbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibbleApp.swift
+//  Nibble
+//
+//  Created by Yashi Surapaneni on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NibbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
